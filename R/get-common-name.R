b6d3037f243e79c40862afb0776e39dd2e03ad69
Stefan Vriend <svriend@gmail.com>
2024-10-28 17:42:39 +0100

    Add option select common name in different languages

--- R/get-common-name.R
+++ R/get-common-name.R
@@ -2,7 +2,7 @@
 
 # Author: Stefan Vriend
 # Created: 2024-10-24
-# Last updated: 2024-10-25
+# Last updated: 2024-10-28
 
 
 # Load packages -----------------------------------------------------------
@@ -12,35 +12,36 @@ library(dplyr)
 library(stringr)
 #library(WikidataR)
 
-# Get English common name through {WikidataQueryServiceR} -----------------
+# Get common name through {WikidataQueryServiceR} -----------------
 
 # Note: this code uses SPARQL (an RDF-query language)
 
 # Arguments
 # sci_name: Character specifying one or more scientific names
+# lang: Character specifying one or more languages the common name
 
-get_common_name <- function(sci_name) {
+get_common_name <- function(sci_name,
+                            lang = "en") {
   
   # SPARQL query to select common name from Wikidata
   query <- paste0('
-      SELECT
-        ?item ?common_name
+      SELECT DISTINCT
+        ?item ?common_name (LANG(?common_name) AS ?lang)
       WHERE {
         ?item wdt:P225', '"', sci_name, '"', ';
               wdt:P1843 ?common_name.
 
-      FILTER(LANGMATCHES(LANG(?common_name), "en"))
+      FILTER(LANGMATCHES(LANG(?common_name), ', '"', lang, '"))
 
       SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}
       ')
   
-  # Send query to Wikidata query service and extract English common name
+  # Send query to Wikidata query service and extract common name
   common_name <- WikidataQueryServiceR::query_wikidata(sparql_query = query,
                                                        format = "smart") |> 
     dplyr::bind_rows() |> 
-    dplyr::pull("common_name") |>
-    stringr::str_to_sentence() |>
-    unique()
+    dplyr::mutate(common_name = stringr::str_to_title(common_name)) |> 
+    dplyr::distinct()
   
   return(common_name)
   
